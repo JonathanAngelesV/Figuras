08d18d34dd80c2764d9da41a0f7b6e2f19815aa6
Jonathan Angeles Valadez <jonathanangeles67@gmail.com>
2016-04-12 21:25:18 -0700

    figuras creadas

---
+++ WindowsFormsApplication1/Figura.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+//Angeles Valadez Jonathan
+
+namespace WindowsFormsApplication1
+{
+    abstract class Figura
+    {
+        public int X, Y;
+        public Pen pluma;
+        public Brush brocha;
+        public int ancho, largo;
+        public Figura(int x, int y)
+        {
+            X = x;
+            Y = y;
+            pluma = new Pen(Color.Green, 3);
+            ancho = 55;
+            largo = 55;
+        }
+
+        public abstract void Dibujar(Form f);
+
+
+
+    }
+    class Rectangulo : Figura
+    {
+        public Rectangulo(int x, int y) : base(x, y)
+        {
+
+        }
+        public override void Dibujar(Form f)
+        {
+            
+            {
+                Graphics g = f.CreateGraphics();
+                g.DrawRectangle(pluma, X, Y, ancho, largo);
+            }
+        }
+        
+
+    }
+    class Circulo : Figura
+    {
+        public Circulo(int x, int y) : base(x, y)
+        {
+
+        }
+        public override void Dibujar(Form f)
+        {
+
+            {
+                Graphics g = f.CreateGraphics();
+                g.DrawEllipse(pluma, X, Y, ancho, largo);
+            }
+        }
+
+
+    }
+
+
+
+
+
+}
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Angeles Valadez Jonathan
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        List<Figura> figuras;
+        public Form1()
+        {
+            figuras = new List<Figura>();
+            InitializeComponent();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(e.Button ==  MouseButtons.Left)
+            {
+                Circulo C= new Circulo(e.X, e.Y);
+                C.Dibujar(this);
+                figuras.Add(C);
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                Rectangulo R = new Rectangulo(e.X, e.Y);
+                R.Dibujar(this);
+                figuras.Add(R);
+            }
+        }
+    }
+}
