c63a0e7010f42c36b58ba3d2e5be1e680bdbf2d6
JonathanAngelesV <jonathanangeles67@gmail.com>
2016-04-13 19:49:01 -0700

    Figuras 2
    Ahora se guardan las coordenadas de las figuras que uno crea al dar click sobre la forma y asi al momento de cerrarla y re-abrirlo, se vuelvan a dibujar las figuras en el mismo lugar.

---
+++ Figuras2/Figuras2/Figuras2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+//Angeles Valadez Jonathan
+
+namespace Figuras2
+{
+   abstract class Figuras2
+    {
+        public int X, Y;
+        public Pen pluma;
+        public Brush brocha;
+        public int ancho, largo;
+        public Figuras2(int x, int y)
+        {
+            X = x;
+            Y = y;
+            pluma = new Pen(Color.Green, 3);
+            ancho = 55;
+            largo = 55;
+        }
+
+        public abstract void Dibujar(Form f);
+
+
+
+    }
+    class Rectangulo : Figuras2
+    {
+        public Rectangulo(int x, int y) : base(x, y)
+        {
+
+        }
+        public override void Dibujar(Form f)
+        {
+
+            {
+                Graphics g = f.CreateGraphics();
+                g.DrawRectangle(pluma, X, Y, ancho, largo);
+            }
+        }
+
+
+    }
+    class Circulo : Figuras2
+    {
+        public Circulo(int x, int y) : base(x, y)
+        {
+
+        }
+        public override void Dibujar(Form f)
+        {
+
+            {
+                Graphics g = f.CreateGraphics();
+                g.DrawEllipse(pluma, X, Y, ancho, largo);
+            }
+        }
+
+
+    }
+
+}
---
+++ Figuras2/Figuras2/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Angeles Valadez Jonathan
+
+namespace Figuras2
+{
+    public partial class Form1 : Form
+    {
+        List<Figuras2> figuras;
+        
+        public Form1()
+        {
+            figuras = new List<Figuras2>();
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Circulo C = new Circulo(e.X, e.Y);
+                C.Dibujar(this);
+                figuras.Add(C);
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                Rectangulo R = new Rectangulo(e.X, e.Y);
+                R.Dibujar(this);
+                figuras.Add(R);
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Figuras2 f in figuras)
+            {
+                f.Dibujar(this);
+            }
+        }
+    }
+}
